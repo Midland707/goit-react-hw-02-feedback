34fe10d73c926b93d9f41e541be537c83d75de41
Midland707 <rus14job@ukr.net>
2023-02-27 02:05:01 +0200

    refactoring code - step3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,85 @@
+import React, { Component } from 'react';
+import { Section } from 'components/Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
+import { Statistics } from 'components/Statistics/Statistics';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <FeedbackOptions title="Please leave feedback" />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positivePercentage: 0,
+  };
+
+  onLeaveFeedback = () => {
+    this.setState(state => ({
+      good: state.good + 1,
+    }));
+    this.countTotalFeedback();
+  };
+
+  countTotalFeedback() {
+    this.setState(state => ({
+      total: state.good + state.neutral + state.bad,
+    }));
+    this.countPositiveFeedbackPercentage();
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    this.setState(state => ({
+      positivePercentage: Number(((100 * state.good) / state.total).toFixed(0)),
+    }));
+  };
+
+  goodStateValueIncrement = () => {
+    this.setState(state => ({
+      good: state.good + 1,
+    }));
+    this.countTotalFeedback();
+  };
+
+  neutralStateValueIncrement = () => {
+    this.setState(state => ({
+      neutral: state.neutral + 1,
+    }));
+    this.countTotalFeedback();
+  };
+
+  badStateValueIncrement = () => {
+    this.setState(state => ({
+      bad: state.bad + 1,
+    }));
+    this.countTotalFeedback();
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          fontSize: 40,
+        }}
+      >
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.state.total}
+            positivePercentage={this.state.positivePercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,86 +1,21 @@
-import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import css from './FeedbackOptions.module.css';
 
-export class FeedbackOptions extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-    positivePercentage: 0,
-  };
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map((state, index) => (
+      <button
+        key={index}
+        className={css.buttonOptions}
+        type="button"
+        onClick={onLeaveFeedback}
+      >
+        {state}
+      </button>
+    ))}
+  </div>
+);
 
-  countTotalFeedback() {
-    this.setState(state => ({
-      total: state.good + state.neutral + state.bad,
-    }));
-    this.countPositiveFeedbackPercentage();
-  }
-
-  countPositiveFeedbackPercentage = () => {
-    this.setState(state => ({
-      positivePercentage: ((100 * state.good) / state.total).toFixed(0),
-    }));
-  };
-
-  goodStateValueIncrement = () => {
-    this.setState(state => ({
-      good: state.good + 1,
-    }));
-    this.countTotalFeedback();
-  };
-
-  neutralStateValueIncrement = () => {
-    this.setState(state => ({
-      neutral: state.neutral + 1,
-    }));
-    this.countTotalFeedback();
-  };
-
-  badStateValueIncrement = () => {
-    this.setState(state => ({
-      bad: state.bad + 1,
-    }));
-    this.countTotalFeedback();
-  };
-
-  render() {
-    const { title } = this.props;
-    return (
-      <div>
-        {title && <h2 className={css.title}>{title}</h2>}
-        <button
-          className={css.buttonOttions}
-          type="button"
-          onClick={this.goodStateValueIncrement}
-        >
-          Good
-        </button>
-        <button
-          className={css.buttonOttions}
-          type="button"
-          onClick={this.neutralStateValueIncrement}
-        >
-          Neutral
-        </button>
-        <button
-          className={css.buttonOttions}
-          type="button"
-          onClick={this.badStateValueIncrement}
-        >
-          Bad
-        </button>
-        <div>
-          <h3 className={css.title}>Statistics</h3>
-          <div className={css.statBlock}>
-            <span>Good: {this.state.good}</span>
-            <span>Neutral: {this.state.neutral}</span>
-            <span>Bad: {this.state.bad}</span>
-            <span>Total: {this.state.total}</span>
-            <span>Positive feedback: {this.state.positivePercentage} %</span>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
